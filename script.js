3763a72eb739f6fa6627e2774a543b62329df326
Todd Wilson <kevs9487@gmail.com>
2022-07-26 14:35:25 -0400

    added player cmd

--- script.js
+++ script.js
@@ -1,16 +1,127 @@
+function teamsInfo(data) {
+    let abbs = []
+    let names = []
+    let name_types = ['locationName', 'name', 'shortName', 'teamName']
+    let rosters = []
+    for (let i=0; i<data['teams'].length; i++) {
+        abbs.push(data['teams'][i]['abbreviation'].toLowerCase());
+        rosters.push(data['teams'][i]['roster']['roster']);
+        for (let j=0; j<name_types.length; j++) {
+            names.push(data['teams'][i][name_types[j]].toLowerCase());
+            
+        }
+    }
+    return {'team_names': names, 'team_abbs': abbs, 'rosters': rosters}
+}
+
+async function matchTeam(name, team) {
+    let obj;
+
+    const res = await fetch('https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster');
+
+    obj = await res.json();
+
+    let teams = teamsInfo(obj);
+    let abbs = teams['team_abbs'];
+    let names = teams['team_names'];
+    let ratio = names.length / abbs.length;
+
+    team = team.toLowerCase();
+    name = name.toLowerCase();
+
+    let index = -1;
+
+    for (let i=0; i<abbs.length; i++ ) {
+        if (abbs[i] == team) {
+            index = i;
+            break;
+        }
+        for (let j=0; j<ratio; j++) {
+            if (names[j + ratio * i] == team) {
+                index = i;
+                break;
+            }
+        }
+    }
+    let id = 0;
+    let roster = teams['rosters'][index]
+    for (let i=0; i<roster.length; i++) {
+        console.log(roster[i]['person']['fullName']);
+        if (name == roster[i]['person']['fullName'].toLowerCase()) {
+            id = roster[i]['person']['id'];
+        }
+    }
+
+    return id;
+
+
+}
+
+function toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+    );
+  }
+
 $('body').terminal({
     cat: function(width, height) {
         return $('<img src="https://placekitten.com/' +
                  width + '/' + height + '">');
     },
-    title: function() {
-        return fetch('https://terminal.jcubic.pl')
-            .then(r => r.text())
-            .then(html => html.match(/<title>([^>]+)<\/title>/)[1]);
-    }, 
     player: function() {
+        this.push(function(name) {
+            this.push(function(last_name) {
+                this.push(function(team) {
+                    name = name.replace(/\s/g, '');
+                    last_name = last_name.replace(/\s/g, '');
+                    matchTeam(name + ' ' + last_name, team)
+                        .then((id) => {
+                            name = toTitleCase(name);
+                            last_name = toTitleCase(last_name);
+                            this.echo('\n' + name + ' ' + last_name)
+                            fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=careerRegularSeason`)
+                            .then(res => res.json())
+                            .then((data) => {
+                                this.echo('\nRegular Season Stats:')
+                                let keys = Object.keys(data['stats'][0]['splits'][0]['stat']);
+                                let key_ind;
+                                for (let i=0; i<keys.length; i++) {
+                                    key_ind = keys[i]
+                                    this.echo('\t' + key_ind + ': ' + data['stats'][0]['splits'][0]['stat'][key_ind]);
+                                }
+                            
+                            })
+                            fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=careerPlayoffs`)
+                            .then(res => res.json())
+                            .then((data) => {
+                                this.echo('\nPlayoff Stats:')
+                                let keys = Object.keys(data['stats'][0]['splits'][0]['stat']);
+                                let key_ind;
+                                for (let i=0; i<keys.length; i++) {
+                                    key_ind = keys[i]
+                                    this.echo('\t' + key_ind + ': ' + data['stats'][0]['splits'][0]['stat'][key_ind]);
+                                }
+                            
+                            })
+                    });
+
+                this.pop().pop().pop();
+            }, {
+            prompt: 'team: '
+        })
         
-    }
+            
+        }, {
+            prompt: 'last name: '
+        })
+    }, {
+        prompt: 'first name: '
+    })
+    }, 
+        
 }, {
     greetings: 'My First JavaScript Terminal\n'
 });
--- test.js
+++ test.js
@@ -2,20 +2,26 @@ let teamsInfo = (data) => {
     let abbs = []
     let names = []
     let name_types = ['locationName', 'name', 'shortName', 'teamName']
+    let rosters = []
     for (let i=0; i<data['teams'].length; i++) {
         abbs.push(data['teams'][i]['abbreviation'].toLowerCase());
+        rosters.push(data['teams'][i]['roster']['roster']);
         for (let j=0; j<name_types.length; j++) {
             names.push(data['teams'][i][name_types[j]].toLowerCase());
             
         }
     }
     console.log(names);
+    console.log(rosters[0][0]['person']['fullName']);
     console.log(abbs);  
 }
 
-fetch('https://statsapi.web.nhl.com/api/v1/teams')
+let tweakData = (data) => {
+    console.log(data['stats'][0]['splits'][0]['stat'])
+}
+fetch('https://statsapi.web.nhl.com/api/v1/people/8474564/stats?stats=careerPlayoffs')
     .then(jsonData => jsonData.json())
-    .then(data => teamsInfo(data))
+    .then(data =>tweakData(data))
 
 
 
